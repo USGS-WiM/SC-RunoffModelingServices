8238d2cf153729ee1545c28a8e2fc3984124855b
amedenblik <amedenblik@usgs.gov>
2022-04-25 10:39:28 -0400

    #15 clean up and add VR limitations

--- Bohman_Method_1989.py
+++ Bohman_Method_1989.py
@@ -34,7 +34,7 @@ def computeRuralFloodHydrographBohman1989(regionBlueRidgePercentArea, regionPied
     regionLowerCoastalPlain1VR = 0.002652 * (A ** -0.953) * (Qp ** 0.978) * (regionLowerCoastalPlain1LT ** 0.882)
     regionLowerCoastalPlain2VR = 0.002872  * (A ** -0.953) * (Qp ** 0.978) * (regionLowerCoastalPlain2LT ** 0.882)
 
-    # Weight the regional Runoff Volume (VR) values
+    # Weighted average Runoff Volume for a rural basin (inches)
     weightedVR = (regionBlueRidgeFractionArea * regionBlueRidgeVR) + \
                  (regionPiedmontFractionArea * regionPiedmontVR) + \
                  (regionUpperCoastalPlainFractionArea * regionUpperCoastalPlainVR) + \
@@ -48,7 +48,7 @@ def computeRuralFloodHydrographBohman1989(regionBlueRidgePercentArea, regionPied
     regionLowerCoastalPlain1LTA = 6.95 * (A ** 0.348) * (Qp ** -0.022)
     regionLowerCoastalPlain2LTA = 11.7 * (A ** 0.348) * (Qp ** -0.022)
 
-    # Weight the Adjusted Lag Time (LTA) values
+    # Weighted Adjusted Lag Time (LTA) 
     weightedLTA = (regionBlueRidgeFractionArea * regionBlueRidgeLTA) + \
                  (regionPiedmontFractionArea * regionPiedmontLTA) + \
                  (regionUpperCoastalPlainFractionArea * regionUpperCoastalPlainLTA) + \
@@ -70,7 +70,7 @@ def computeRuralFloodHydrographBohman1989(regionBlueRidgePercentArea, regionPied
                                             0.14, 0.13, 0.12, 0.12, 0.11,
                                             0.10, 0.10, 0.09 ]
     regionBlueRidgeDischargeRatio = np.asarray(regionBlueRidgeDischargeRatioList)
-    regionPiedmontDischargeRatioList =     [ 0.07, 0.09, 0.11, 0.14, 0.17,
+    regionPiedmontDischargeRatioList =    [ 0.07, 0.09, 0.11, 0.14, 0.17,
                                             0.21, 0.25, 0.30, 0.37, 0.44,
                                             0.53, 0.61, 0.70, 0.78, 0.86,
                                             0.92, 0.96, 0.99, 1.00, 0.98,
@@ -78,7 +78,7 @@ def computeRuralFloodHydrographBohman1989(regionBlueRidgePercentArea, regionPied
                                             0.69, 0.63, 0.58, 0.53, 0.49,
                                             0.44, 0.41, 0.37, 0.34, 0.32,
                                             0.29, 0.27, 0.25, 0.23, 0.21,
-                                            0.19, 0.1, 0.16, 0.15, 0.13,
+                                            0.19, 0.18, 0.16, 0.15, 0.13,
                                             0.12, 0.11, 0.10 ]
     regionPiedmontDischargeRatio = np.asarray(regionPiedmontDischargeRatioList)
     regionCoastalPlainDischargeList =     [ 0.07, 0.10, 0.14, 0.18, 0.23,
@@ -108,27 +108,59 @@ def computeRuralFloodHydrographBohman1989(regionBlueRidgePercentArea, regionPied
     dischargeCoordinates = dischargeRatio * Qp
 
     # Show the scatter plot of the rural flood hydrograph (for testing)
-    #plt.scatter(timeCoordinates, dischargeCoordinates)
-    #plt.show()
+    # plt.scatter(timeCoordinates, dischargeCoordinates)
+    # plt.show()
 
-    # Check limitations of method from Table 15
+    ## Check limitations from Table 15
     warningMessage = "These methods are not applicable to streams where regulation, urbanization, temporary in-channel storage, or overbank detention storage is significant."
-    isWarning = False
+
+    # Check limitations for average basin lagtime
+    warningMessageLT = "One or more of the parameters is outside the suggested range; basin lagtime was estimated with unknown errors."
     if regionBlueRidgePercentArea > 0:
         if A < 2.83 or A > 455:
-            isWarning = True
+            warningMessage += warningMessageLT
     if regionPiedmontPercentArea > 0:
         if A < 0.52 or A > 444:
-            isWarning = True
+            warningMessage += warningMessageLT
     if regionUpperCoastalPlainPercentArea > 0:
         if A < 2.92 or A > 401:
-            isWarning = True
+            warningMessage += warningMessageLT
     if regionLowerCoastalPlain1PercentArea > 0 or regionLowerCoastalPlain2PercentArea > 0:
         if A < 7.67 or A > 401:
-            isWarning = True
+            warningMessage += warningMessageLT
 
-    if isWarning:
-        warningMessage = "One or more of the parameters may be outside the suggested range. Estimates were extrapolated with unknown errors. " + warningMessage
+    # Check limitations for runoff volume
+    warningMessageVR = "One or more of the parameters is outside the suggested range; runoff volume was estimated with unknown errors."
+    if regionBlueRidgePercentArea > 0:
+        if A < 30.2 or A > 455:
+            warningMessage += warningMessageVR
+        if Qp < 231 or Qp > 12800:
+            warningMessage += warningMessageVR
+        if regionBlueRidgeLT < 8.77 or regionBlueRidgeLT > 19.6:
+            warningMessage += warningMessageVR
+    if regionPiedmontPercentArea > 0:
+        if A < 0.52 or A > 444:
+            warningMessage += warningMessageVR
+        if Qp < 2.94 or Qp > 16400:
+            warningMessage += warningMessageVR
+        if regionPiedmontLT < 1.92 or regionPiedmontLT > 50.2:
+            warningMessage += warningMessageVR
+    if regionUpperCoastalPlainPercentArea > 0:
+        if A < 2.92 or A > 122:
+            warningMessage += warningMessageVR
+        if Qp < 10.4 or Qp > 625:
+            warningMessage += warningMessageVR
+        if regionUpperCoastalPlainLT < 9.88 or regionUpperCoastalPlainLT > 49.7:
+            warningMessage += warningMessageVR
+    if regionLowerCoastalPlain1PercentArea > 0 or regionLowerCoastalPlain2PercentArea > 0:
+        if A < 7.67 or A > 401:
+            warningMessage += warningMessageVR
+        if Qp < 16.7 or Qp > 2560:
+            warningMessage += warningMessageVR
+        if regionLowerCoastalPlain1LT < 11.7 or regionLowerCoastalPlain1LT > 95.5 or regionLowerCoastalPlain2LT < 11.7 or regionLowerCoastalPlain2LT > 95.5:
+            warningMessage += warningMessageVR
 
-    ## Note: if Runoff Volume is added to the variables returned, check limitations related to runoff volume in Table 15
-    return timeCoordinates.tolist(), dischargeCoordinates.tolist(), warningMessage
+    warningMessage += "These methods are not applicable to streams where regulation, urbanization, temporary in-channel storage, or overbank detention storage is significant."
+
+
+    return weightedVR, timeCoordinates.tolist(), dischargeCoordinates.tolist(), warningMessage
