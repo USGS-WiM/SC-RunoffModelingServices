e3dab9d8e1a081c7f2fb6674807c190fccbd0117
amedenblik <amedenblik@usgs.gov>
2023-01-26 15:25:19 -0500

    #1 revert root_path

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from Tc_Calculator import lagTimeMethodTimeOfConcentration, travelTimeMethodTime
 
 app = FastAPI(
     title='SC Runoff Modeling Services',
-    # root_path='/local/scrunoffservices'
+    root_path='/local/scrunoffservices'
     # To run locally use
-    root_path=''
+    # root_path=''
     # To run in production use
     #    root_path='/local/scrunoffservices'
 )
