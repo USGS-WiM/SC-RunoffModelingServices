995f761f80409513b976300eedba85267c7aabfa
Wavra <hwavra@usgs.gov>
2023-02-21 15:54:02 -0600

    #55 - calcualte Y2

--- Storm_Ponds.py
+++ Storm_Ponds.py
@@ -61,65 +61,66 @@ def calcponds():
     A = [] # Y-S (1)
     change_S = [] # Pond_1 hr
     twoS_dtplusQ = [] # Y-S (1)
-
+    Y = []
     # Caluclate values in Y-Q (1) sheet
     Orif1_A = 3.14159*((Orif1_Dia/12)**2)/4
     Orif2_A = 3.14159*((Orif2_Dia/12)**2)/4
-    for Y in range(0, max_depth+1):
-        h = pond_bottom_elev + Y
+    for y in range(0, max_depth+1):
+        Y.append(y)
+        h = pond_bottom_elev + y
         # First Stage Orfice Total Flow
-        Orif1_H = max(0,Y-Orif1_CtrEL)
+        Orif1_H = max(0,y-Orif1_CtrEL)
         Orif1_Q = max(0,Orif1_Coeff*Orif1_A*math.sqrt(64.4*Orif1_H))   
         Orif1_total_flow = Orif1_NumOpenings*Orif1_Q
         # Second Stage Orfice Total Flow
-        Orif2_H = max(0,Y-Orif2_CtrEL)
+        Orif2_H = max(0,y-Orif2_CtrEL)
         Orif2_Q = max(0,Orif2_Coeff*Orif2_A*math.sqrt(64.4*Orif2_H)) 
         Orif2_total_flow = Orif2_NumOpenings*Orif2_Q
         # Upper Stage Rectangular Weir Total Flow
-        Rec_Weir_H = max(0,Y-Rec_WeirCrest_EL)
+        Rec_Weir_H = max(0,y-Rec_WeirCrest_EL)
         Rec_Single_Weir_Q = Rec_Weir_Coeff*Rec_Weir_Length*Rec_Weir_H**Rec_Weir_Ex
         Rec_Stage_total_flow = Rec_Single_Weir_Q*Rec_Num_Wiers
         # Overflow Spillway Q
-        OS_H = max(0,Y-OS_Crest_EL)
+        OS_H = max(0,y-OS_Crest_EL)
         OS_Q = OS_BCWeir_Coeff*OS_Length*OS_H**OS_Weir_Ex
         # Seepage in cfs
-        if Y == 0:
+        if y == 0:
             Total_L.append(length+2*bottom_slope*(h-pond_bottom_elev))
             Total_W1.append(w1+2*side_slope_z*(h-pond_bottom_elev))
             Total_W2.append(w2+2*side_slope_z*(h-pond_bottom_elev))
-            A1 = Total_L[Y]*(Total_W1[Y]+Total_W2[Y])/2
+            A1 = Total_L[y]*(Total_W1[y]+Total_W2[y])/2
             A.append(A1)
         else:
-            Total_L.append(Total_L[Y-1]+2*side_slope_z*(1)) # not sure if it will always be (1)
-            Total_W1.append(Total_W1[Y-1]+2*side_slope_z*(1)) # not sure if it will always be (1)
-            Total_W2.append(Total_W2[Y-1]+2*side_slope_z*(1)) # not sure if it will always be (1)
-            A.append(Total_L[Y]*(Total_W1[Y]+Total_W2[Y])/2)
-        if Y == 0:
+            Total_L.append(Total_L[y-1]+2*side_slope_z*(Y[y] - Y[y-1])) 
+            Total_W1.append(Total_W1[y-1]+2*side_slope_z*(Y[y] - Y[y-1]))
+            Total_W2.append(Total_W2[y-1]+2*side_slope_z*(Y[y] - Y[y-1]))
+            A.append(Total_L[y]*(Total_W1[y]+Total_W2[y])/2)
+        if y == 0:
             Seepage_Bottom_CFS = 0
             Seepage_Side_CFS = 0
         else:
             Seepage_Bottom_CFS = A1*Seepage_Bottom/(12*3600)
-            Seepage_Side_CFS = (A[Y]-A1)*Seepage_Side/(12*3600)
+            Seepage_Side_CFS = (A[y]-A1)*Seepage_Side/(12*3600)
         
         # Calculate Q in 2S_Dt+Q & Pond_X hr sheets
         Outflow_Q = Orif1_total_flow+Orif2_total_flow+Rec_Stage_total_flow+OS_Q+Seepage_Bottom_CFS+Seepage_Side_CFS
         Q.append(Outflow_Q) 
         # Calculate S in 2S_Dt+Q & Pond_X hr sheets; Calculate change in S from Y-S (1) sheet 
-        if Y == 0:
+        if y == 0:
             S.append(0)
             change_S.append(0)
         else:
-            change_S.append(((A[Y-1]+A[Y])/2)*(1)) # not sure if it will always be (1)
-            S.append(S[Y-1] + change_S[Y])
+            change_S.append(((A[y-1]+A[y])/2)*(Y[y] - Y[y-1])) 
+            S.append(S[y-1] + change_S[y])
         # Calculate 2S/dt+Q in 2S_Dt+Q & Pond_X hr sheets
-        twoS_dtplusQ.append((2*S[Y]/(60*burst_duration))+Q[Y])
+        twoS_dtplusQ.append((2*S[y]/(60*burst_duration))+Q[y])
 
         
 
     print(Q) 
     print(S)
     print(twoS_dtplusQ)
-   
+    print('Y:', Y)
 
     # TODO Loop thorugh storm_duration
     # Initlaize arrays used in 
@@ -146,30 +147,40 @@ def calcponds():
             i1plusi2.append(inflow[counter-1] + inflow[counter])
             twoS_dtminusQ1.append(twoS_dtplusQ2[counter-1]-2*Q2[counter-1])
             twoS_dtplusQ2.append(i1plusi2[counter]+twoS_dtminusQ1[counter])
-            # Q2
+            # Q2 & Y2
             if (twoS_dtplusQ2[counter]<twoS_dtplusQ[1]):
                 Q2.append(Q[0]+((Q[1]-Q[0])/(twoS_dtplusQ[1]-twoS_dtplusQ[0]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[0]))
+                Y2.append(Y[0]+((Y[1]-Y[0])/(twoS_dtplusQ[1]-twoS_dtplusQ[0]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[0]))
             elif (twoS_dtplusQ2[counter]<twoS_dtplusQ[2]):  
                 Q2.append(Q[1]+((Q[2]-Q[1])/(twoS_dtplusQ[2]-twoS_dtplusQ[1]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[1]))
+                Y2.append(Y[1]+((Y[2]-Y[1])/(twoS_dtplusQ[2]-twoS_dtplusQ[1]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[1]))
             elif (twoS_dtplusQ2[counter]<twoS_dtplusQ[3]):  
                 Q2.append(Q[2]+((Q[3]-Q[2])/(twoS_dtplusQ[3]-twoS_dtplusQ[2]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[2]))
+                Y2.append(Y[2]+((Y[3]-Y[2])/(twoS_dtplusQ[3]-twoS_dtplusQ[2]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[2]))
             elif (twoS_dtplusQ2[counter]<twoS_dtplusQ[4]):                 
                 Q2.append(Q[3]+((Q[4]-Q[3])/(twoS_dtplusQ[4]-twoS_dtplusQ[3]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[3]))
+                Y2.append(Y[3]+((Y[4]-Y[3])/(twoS_dtplusQ[4]-twoS_dtplusQ[3]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[3]))
             elif (twoS_dtplusQ2[counter]<twoS_dtplusQ[5]):  
                 Q2.append(Q[4]+((Q[5]-Q[4])/(twoS_dtplusQ[5]-twoS_dtplusQ[4]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[4]))
+                Y2.append(Y[4]+((Y[5]-Y[4])/(twoS_dtplusQ[5]-twoS_dtplusQ[4]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[4]))
             elif (twoS_dtplusQ2[counter]<twoS_dtplusQ[6]):  
                 Q2.append(Q[5]+((Q[6]-Q[5])/(twoS_dtplusQ[6]-twoS_dtplusQ[5]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[5]))
+                Y2.append(Y[5]+((Y[6]-Y[5])/(twoS_dtplusQ[6]-twoS_dtplusQ[5]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[5]))
             elif (twoS_dtplusQ2[counter]<twoS_dtplusQ[7]):  
                 Q2.append(Q[6]+((Q[7]-Q[6])/(twoS_dtplusQ[7]-twoS_dtplusQ[6]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[6]))
+                Y2.append(Y[6]+((Y[7]-Y[6])/(twoS_dtplusQ[7]-twoS_dtplusQ[6]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[6]))
             elif (twoS_dtplusQ2[counter]<twoS_dtplusQ[8]): 
                 Q2.append(Q[7]+((Q[8]-Q[7])/(twoS_dtplusQ[8]-twoS_dtplusQ[7]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[7])) 
+                Y2.append(Y[7]+((Y[8]-Y[7])/(twoS_dtplusQ[8]-twoS_dtplusQ[7]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[7])) 
             elif (twoS_dtplusQ2[counter]<twoS_dtplusQ[9]):  
-                Q2.append(Q[8]+((Q[9]-Q[8])/(twoS_dtplusQ[9]-twoS_dtplusQ[8]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[8])) 
+                Q2.append(Q[8]+((Q[9]-Q[8])/(twoS_dtplusQ[9]-twoS_dtplusQ[8]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[8]))
+                Y2.append(Y[8]+((Y[9]-Y[8])/(twoS_dtplusQ[9]-twoS_dtplusQ[8]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[8]))  
             elif (twoS_dtplusQ2[counter]<twoS_dtplusQ[10]): 
                 Q2.append(Q[9]+((Q[10]-Q[9])/(twoS_dtplusQ[10]-twoS_dtplusQ[9]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[9])) 
+                Y2.append(Y[9]+((Y[10]-Y[9])/(twoS_dtplusQ[10]-twoS_dtplusQ[9]))*(twoS_dtplusQ2[counter]-twoS_dtplusQ2[9])) 
             else:
                 Q2.append('error') 
-            # Y2
+                Y2.append('error')
         counter = counter + 1
 
     print('time:', time)
@@ -178,6 +189,7 @@ def calcponds():
     print('twoS_dtminusQ1:', twoS_dtminusQ1)
     print('twoS_dtplusQ2:', twoS_dtplusQ2)
     print('Q2:', Q2)
+    print('Y2:', Y2)
 
     # Corresponds red arrow in the "D-hr Storm Pond Results" sheet
     #index_max_peak_outflow = np.argmax(pond_peak_outflow)
